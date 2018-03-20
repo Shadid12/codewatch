7c625c177726baac1f3a30e5e56800a06c3fb344
shadid12 <shadidhaque2012@gmail.com>
2018-03-20 18:27:10 -0400

    initial page setup is done

---
+++ server.js
@@ -0,0 +1,27 @@
+const express       = require('express');
+const bodyParser    = require('body-parser');
+const ejs           = require('ejs');
+const http          = require('http');
+const container     = require('./container');
+
+container.resolve(function(users){
+    const app = SetupExpress();
+
+    function SetupExpress(){
+        const app = express();
+        const server = http.createServer(app);
+        server.listen(3000, () => {
+            console.log('Listening on port 3000');
+        });
+
+        ConfigureExpress(app);
+        users.SetRouting(app);
+    }
+
+    function ConfigureExpress(app){
+        app.use(express.static('public'));
+        app.set('view engine', 'ejs');
+        app.use(bodyParser.json());
+        app.use(bodyParser.urlencoded({extended: true}));
+    }
+});
